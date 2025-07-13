eecf456ba0e1df214a4fefcc9ee750b1c51aa461
Sorrow <115921942+rrrrr22@users.noreply.github.com>
2025-07-13 04:49:55 +0300

    working challenge chest

--- ChallengeChest.cs
+++ ChallengeChest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
-using Origins.Graphics.Primitives;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.Enums;
 using Terraria.GameContent;
+using Terraria.GameContent.LootSimulation;
 using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -17,10 +19,6 @@ using Terraria.ObjectData;
 
 namespace CthulhuRealm
 {
-	public class ChallengeEntity
-	{
-		ModTileEntity tileEntity = null;
-	}
 	public class ChallengeSpawnRateIncrease : GlobalNPC
 	{
 		public static bool isActive = false;
@@ -29,7 +27,7 @@ namespace CthulhuRealm
 			if (!isActive) 
 				return;
             maxSpawns *= 3;
-			spawnRate /= 3;
+			spawnRate /= 10;
         }
 	}
     public class ChallengeChest : ModTileEntity
@@ -50,6 +48,8 @@ namespace CthulhuRealm
 			Projectile.NewProjectileDirect(null,Position.ToWorldCoordinates() + new Vector2(800,0),new Vector2(-20,0),ModContent.ProjectileType<FlameWall>(),1,0,-1,1);
 			Projectile.NewProjectileDirect(null,Position.ToWorldCoordinates() - new Vector2(800,0),new Vector2(20,0),ModContent.ProjectileType<FlameWall>(),1,0,-1,0);
 			ChallengeSpawnRateIncrease.isActive = true;
+			Main.NewText("Survive for 60 Seconds, then the Chest opens!",Color.Yellow);
+			Main.SkipToTime(0, false);
 		}
         public override void Update()
         {
@@ -60,6 +60,14 @@ namespace CthulhuRealm
 					isChallengeActive=false;
 					isChallengeCompleted=true;
 					ChallengeSpawnRateIncrease.isActive = false;
+					for(int i = 0; i < 64; i++)
+						Dust.NewDustPerfect(Position.ToWorldCoordinates(),DustID.AmberBolt,Main.rand.NextVector2CircularEdge(12,12),newColor: Color.Orange).noGravity = true;
+					foreach(NPC npc in Main.ActiveNPCs)
+						if(npc.life > 5 && npc.damage > 0)
+						{
+							for(int i = 0; i < 64; i++)
+								Dust.NewDustPerfect(npc.Center,DustID.AmberBolt,Main.rand.NextVector2CircularEdge(4,4),newColor: Color.Orange).noGravity = true;
+						}
 				}
 				timer--;
 			}
@@ -102,21 +110,17 @@ namespace CthulhuRealm
 			// When the tile is removed, we need to remove the Tile Entity as well.
 			ModContent.GetInstance<ChallengeChest>().Kill(i, j);
 		}
-		public override bool LockChest(int i, int j, ref short frameXAdjustment, ref bool manual) {
-			if (frameXAdjustment == 36 * 3) {
-				return true;
-			}
-			return false;
-		}
-		public override bool IsLockedChest(int i, int j) {
-			return Main.tile[i, j].TileFrameX / (36 * 3) == 1;
-		}
+        public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+			if(!TileEntity.TryGet(i,j,out ChallengeChest entity))
+				return false;
 
-		public override bool UnlockChest(int i, int j, ref short frameXAdjustment, ref int dustType, ref bool manual) {
-			if(!TileEntity.TryGet(i, j, out ChallengeChest tileEntity))
-				return false;			
-			return tileEntity.isChallengeCompleted;
-		}
+			if(!entity.isChallengeCompleted)
+				return true;
+			Vector2 pos = entity.Position.ToWorldCoordinates() - Main.screenPosition - new Vector2(-200,-164 + MathF.Sin((float)Main.timeForVisualEffects * 0.04f) * 16);
+            spriteBatch.Draw(ModContent.Request<Texture2D>("Terraria/Images/UI/ButtonPlay").Value,pos,null,Color.White,MathHelper.PiOver2,new Vector2(11,11),1f,SpriteEffects.None,0f);
+            return base.PreDraw(i, j, spriteBatch);
+        }
 
         public override bool RightClick(int i, int j)
         {
@@ -127,9 +131,22 @@ namespace CthulhuRealm
 				tileEntity.ActivateChallenge();
 				return true;		
 			}
+			if(tileEntity.isChallengeCompleted)
+			{
+				Main.LocalPlayer.QuickSpawnItem(null,ItemID.TerraBlade);
+				WorldGen.KillTile(i,j);
+            }
             return false;
         }
     }
+
+	public class WhyIhaveToDoThisTileGlobal : GlobalTile 
+	{
+        public override bool CanDrop(int i, int j, int type)
+        {
+            return type != ModContent.TileType<ChallengeChestTile>();
+        }
+	}
 
 	public class DebugChallengeChest : ModItem
 	{
@@ -146,8 +163,8 @@ namespace CthulhuRealm
         public override string Texture => "Terraria/Images/Projectile_0";
 		public override void SetDefaults() 
 		{
-			Projectile.width = 32;
-			Projectile.height = 1024;
+			Projectile.width = 128;
+			Projectile.height = 2048;
 			Projectile.aiStyle = -1;
 			Projectile.hostile = true;
 			Projectile.friendly = false;
@@ -158,6 +175,10 @@ namespace CthulhuRealm
         public override void AI()
         {
             Projectile.velocity *= 0.9f;
+			foreach(Player player in Main.ActivePlayers)
+				if(player.getRect().Intersects(Projectile.Hitbox))
+					player.KillMe(PlayerDeathReason.ByProjectile(player.whoAmI,Projectile.whoAmI),999999,0);
+
         }
         public override void OnHitPlayer(Player target, Player.HurtInfo info)
         {
@@ -165,7 +186,16 @@ namespace CthulhuRealm
         }
         public override bool PreDraw(ref Color lightColor)
         {
-			default(WallofFlamesShader).Draw(Projectile.Center,MathHelper.Lerp(1,0,Projectile.timeLeft / 200));
+
+			float progress = 1;
+
+			if(Projectile.timeLeft > 3400)
+				progress = MathHelper.Lerp(1,0,(float)(Projectile.timeLeft - 3400) / 200);
+			if(Projectile.timeLeft <= 200)
+				progress = MathHelper.Lerp(0,1,(float)(Projectile.timeLeft) / 200);
+
+			default(WallofFlamesShader).Draw(Projectile.Center,progress);
+			
             return base.PreDraw(ref lightColor);
         }
 		struct WallofFlamesShader 
@@ -174,9 +204,9 @@ namespace CthulhuRealm
 			public void Draw(Vector2 position,float progress)
 			{
 				MiscShaderData shader = GameShaders.Misc["CR:WallofFlames"].UseImage1(TextureAssets.Extra[193]);
-				shader.UseColor(Color.Orange);
-				shader.UseSecondaryColor(Color.DarkRed);
-				shader.UseShaderSpecificData(new Vector4());
+				shader.UseColor(Color.Red);
+				shader.UseSecondaryColor(Color.Crimson);
+				shader.UseShaderSpecificData(new Vector4(progress));
 				shader.Apply();
 				rect.Draw(position - Main.screenPosition,size: new Vector2(256,2000));
 
--- WallofFlames.fx
+++ WallofFlames.fx
@@ -4,7 +4,7 @@ sampler2D image2 : register(s2);
 sampler2D image3 : register(s3);
 
 float uTime;
-float4 uShaderData;
+float4 uShaderSpecificData;
 float3 uColor;
 float3 uSecondaryColor;
 
@@ -21,6 +21,8 @@ float4 Flames(float4 sampleColor : COLOR0, float2 coords : TEXCOORD0) : COLOR0
     float4 color1 = noiseTex;
     color1.rgba *= (1 / (distance(uv.x, float2(0.5, uv.x)) * 10));
     color1.rgb *= lerp(uSecondaryColor,uColor,uv.x);
+    color1.a *= 2;
+    color1.rgba *= uShaderSpecificData.x;
     color1 *= 1 / length(uv.x - 0.5) * 0.5;
     return floor(color1 * 5) / 5;
 }
