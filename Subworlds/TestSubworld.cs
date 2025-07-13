4056b736bffa5784ad28ad6a9abe6cc7d86c9550
LowQualityTrashXinim <LELwhyyoulookingatthis@gmail.com>
2025-07-13 08:48:59 +0700

    Added 2 of the libs, modify utils code, test subworld

---
+++ Subworlds/TestSubworld.cs
@@ -0,0 +1,21 @@
+﻿using SubworldLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+
+namespace CthulhuRealm.Subworlds;
+
+public class TestSubworld_System : Subworld
+{
+    public override int Width => 1000;
+
+    public override int Height => 500;
+
+    public override List<GenPass> Tasks => new List<GenPass>
+    {
+    };
+}
